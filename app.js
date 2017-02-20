bc3523bc9b18002dc7dbcbd35cfce62e611810a1
Angie.Visnesky <phoenixtat35@yahoo.com>
2017-02-20 15:13:58 -0500

    Corrective commit, remove .git file

---
+++ app.js
@@ -0,0 +1,71 @@
+'use strict';
+
+try {
+  require('./env.js');
+} catch(error) {
+  // no env file in production environment
+}
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+// // serve static files in the public directory
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/views'));
+
+// pug template engine (formerly Jade)
+// app.set('view engine','html');
+// app.set('views', __dirname + '/views');
+
+app.get('/', function(req,res) {
+  res.sendFile('index.html');
+});
+
+app.post('/send', function(req,res) {
+  console.log(req.body.name);
+  const transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: {
+      user: process.env.USER,
+      pass: process.env.PASS
+    }
+  });
+  const options = {
+    from: 'SmellydogCodingNoReply@gmail.com',
+    to: req.body.email,
+    cc: 'ipaint4u@gmail.com',
+    subject: req.body.subject,
+    text: 'This is a message from ' + req.body.name + ': ' + req.body.message
+  }
+  transporter.sendMail(options, function(error, info) {
+    if (error) {
+        console.log(error);
+        res.send('Uh Oh!  Something went wrong!  Please click refresh and try again.')
+    } else {
+        console.log('Message sent: ' + info.response);
+        res.send('Message successfully sent!  We will be contacting you shortly.');
+    };
+  });
+});
+
+// error pages
+app.use(function (req, res, next) {
+  res.status(404).redirect('/');
+  // res.status(404).render('error',{status: '404', text1: 'Oh hai!  Sorry I couldn\'t find that page that you were looking for.', text2: 'But if you want to click the back button and try something else that\'s cool too.', title: "404"});
+});
+
+// app.use(function (err, req, res, next) {
+//   res.status(500).render('error',{status: '500', text1: 'Uh oh.  It looks like something went wrong.  Just wait a couple minutes and try again.', text2: 'If you keep ending up back here, please tell Smellydog about it.', title: "500"});
+//   console.log(err);
+// });
+
+// server
+app.listen(port, function() {
+  console.log('frontend server is running on port ' + port);
+});
